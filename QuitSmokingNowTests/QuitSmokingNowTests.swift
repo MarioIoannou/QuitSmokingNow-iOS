4369fe457e7fc926666a5c65032762ac9a441adf
mariosioannou <mariosiliasioannou@gmail.com>
2025-03-07 20:41:14 +0200

    Initial Commit

---
+++ QuitSmokingNowTests/QuitSmokingNowTests.swift
@@ -0,0 +1,17 @@
+//
+//  QuitSmokingNowTests.swift
+//  QuitSmokingNowTests
+//
+//  Created by Marios Ioannou on 7/3/25.
+//
+
+import Testing
+@testable import QuitSmokingNow
+
+struct QuitSmokingNowTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
